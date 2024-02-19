41247cb3e3b783ae9ccea17001f2aeab59ab9ff9
darkmoont <grishgautam03@gmail.com>
2024-02-19 22:30:30 +0530

    timer added

--- js/first.js
+++ js/first.js
@@ -7,3 +7,27 @@ $(document).ready(function(){
         nextArrow:".next-btn",
     });
 });
+
+//timer starts here
+var dest= new Date("mar 31,2024 12:00:00").getTime();
+
+var x = setInterval(function(){
+
+var now= new Date().getTime();
+var diff=dest - now;//in milli second
+//now converting it into days,hrs,mins and second
+
+var days = Math.floor(diff/(60*60*24*1000));
+
+var hours = Math.floor((diff %(1000*60*60*24)) / (1000*60*60));
+
+var minutes = Math.floor((diff % (1000*60*60)) / (1000*60));
+
+var second = Math.floor((diff % (1000*60)) / 1000); 
+
+document.getElementById("day").innerHTML = days; 
+document.getElementById("hrs").innerHTML = hours;
+document.getElementById("mins").innerHTML = minutes;
+document.getElementById("sec").innerHTML = second;
+
+}, 1000 );
